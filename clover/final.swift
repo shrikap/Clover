8a67af7fc3b9bff12ae68cb4bf58598ade292012
shrikap <shrikapunuganti@gmail.com>
2023-07-21 10:43:46 -0700

    l

--- clover/final.swift
+++ clover/final.swift
@@ -8,19 +8,24 @@
 import SwiftUI
 struct final: View {
   var body: some View {
-    ZStack {
-      Image("quizlast")
-        .resizable(resizingMode: .stretch)
-        .ignoresSafeArea()
-        VStack{
-            NavigationLink(destination: homepage()) {
-                Image("homebutton")
-                    .resizable(resizingMode: .stretch)
-                .aspectRatio(contentMode: .fit)}
-            Spacer(minLength:750)
-                .ignoresSafeArea()
-        }
-    }
+      NavigationStack{
+          ZStack {
+              
+              Image("quizlast")
+                  .resizable(resizingMode: .stretch)
+                  .ignoresSafeArea()
+              
+              VStack{
+                  NavigationLink(destination: homepage()) {
+                      Image("homebutton")
+                          .resizable(resizingMode: .stretch)
+                      .aspectRatio(contentMode: .fit)}
+                  Spacer(minLength:750)
+                      .ignoresSafeArea()
+              }
+          }
+      }
+        
   }
   struct final_Previews: PreviewProvider {
     static var previews: some View {
